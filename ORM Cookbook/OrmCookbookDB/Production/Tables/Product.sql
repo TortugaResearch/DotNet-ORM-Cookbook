8a68a87704f6af27aba1118a81ea902ca8e397dd
Jonathan Allen <grauenwolf@gmail.com>
2020-01-08 14:12:56 -0800

    Database setup. Work in progress.

---
+++ ORM Cookbook/OrmCookbookDB/Production/Tables/Product.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE Production.Product
+(
+    ProductKey INT NOT NULL IDENTITY(1000, 1)
+        CONSTRAINT PK_Product PRIMARY KEY,
+    ProductName NVARCHAR(50) NOT NULL,
+    ProductLineKey INT NOT NULL
+        CONSTRAINT FK_Product_ProductLineKey
+        REFERENCES Production.ProductLine (ProductLineKey),
+    ShippingWeight NUMERIC(10, 4) NULL,
+    ProductWeight NUMERIC(10, 4) NULL,
+    CONSTRAINT C_Product_Weight CHECK (ShippingWeight IS NULL
+                                       OR ProductWeight IS NULL
+                                       OR ShippingWeight <= ProductWeight
+                                      )
+);
---
+++ ORM Cookbook/OrmCookbookDB/Production/Tables/ProductLine.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Production.ProductLine
+(
+    ProductLineKey INT NOT NULL IDENTITY(1000, 1)
+        CONSTRAINT PK_ProductLine PRIMARY KEY,
+    ProductLineName NVARCHAR(50) NOT NULL
+        CONSTRAINT UX_ProductLine_ProductLineName
+        UNIQUE
+);
