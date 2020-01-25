77b93bb30c329ee2100427e1390c4cccd2674104
Jonathan Allen <grauenwolf@gmail.com>
2020-01-24 17:38:39 -0800

    Implement Row Counts for NHibernate #145 Implment Pagination for NHibernate #141 Fix NHibernate configuration

---
+++ ORM Cookbook/Recipes.NHibernate/Pagination/PaginationTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Recipes.NHibernate.Entities;
+using Recipes.Pagination;
+
+namespace Recipes.NHibernate.Pagination
+{
+    [TestClass]
+    public class PaginationTests : PaginationTests<Employee>
+    {
+        protected override IPaginationScenario<Employee> GetScenario()
+        {
+            return new PaginationScenario(Setup.SessionFactory);
+        }
+    }
+}
