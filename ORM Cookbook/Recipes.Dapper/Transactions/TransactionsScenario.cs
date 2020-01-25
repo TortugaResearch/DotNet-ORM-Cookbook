212ad21563e19cb4e6e99034a7864c5110b0ac36
Jonathan Allen <grauenwolf@gmail.com>
2020-01-24 16:41:07 -0800

    Use Case: Transaction Commit/Rollback #35 ADO, Chain, Dapper, LLBLGen Pro, LINQ2DB, EF Core, NHibernate

---
+++ ORM Cookbook/Recipes.Dapper/Transactions/TransactionsScenario.cs
@@ -0,0 +1,71 @@
+﻿using Dapper;
+using Recipes.Dapper.Models;
+using Recipes.Transactions;
+using System;
+using System.Data;
+
+namespace Recipes.Dapper.Transactions
+{
+    public class TransactionsScenario : ScenarioBase, ITransactionsScenario<EmployeeClassification>
+    {
+        public TransactionsScenario(string connectionString) : base(connectionString)
+        {
+        }
+
+        public int Create(EmployeeClassification classification, bool shouldRollBack)
+        {
+            if (classification == null)
+                throw new ArgumentNullException(nameof(classification), $"{nameof(classification)} is null.");
+
+            var sql = @"INSERT INTO HR.EmployeeClassification (EmployeeClassificationName)
+                        OUTPUT Inserted.EmployeeClassificationKey
+                        VALUES(@EmployeeClassificationName )";
+
+            using (var con = OpenConnection())
+            using (var trans = con.BeginTransaction())
+            {
+                var result = con.ExecuteScalar<int>(sql, classification, transaction: trans);
+
+                if (shouldRollBack)
+                    trans.Rollback();
+                else
+                    trans.Commit();
+
+                return result;
+            }
+        }
+
+        public int CreateWithIsolationLevel(EmployeeClassification classification, bool shouldRollBack, IsolationLevel isolationLevel)
+        {
+            if (classification == null)
+                throw new ArgumentNullException(nameof(classification), $"{nameof(classification)} is null.");
+
+            var sql = @"INSERT INTO HR.EmployeeClassification (EmployeeClassificationName)
+                        OUTPUT Inserted.EmployeeClassificationKey
+                        VALUES(@EmployeeClassificationName )";
+
+            using (var con = OpenConnection())
+            using (var trans = con.BeginTransaction(isolationLevel))
+            {
+                var result = con.ExecuteScalar<int>(sql, classification, transaction: trans);
+
+                if (shouldRollBack)
+                    trans.Rollback();
+                else
+                    trans.Commit();
+
+                return result;
+            }
+        }
+
+        public EmployeeClassification? GetByKey(int employeeClassificationKey)
+        {
+            var sql = @"SELECT ec.EmployeeClassificationKey, ec.EmployeeClassificationName
+                        FROM HR.EmployeeClassification ec
+                        WHERE ec.EmployeeClassificationKey = @EmployeeClassificationKey;";
+
+            using (var con = OpenConnection())
+                return con.QuerySingleOrDefault<EmployeeClassification>(sql, new { employeeClassificationKey });
+        }
+    }
+}
