b93af03d9dbf203c3ba7b519ea274a2ca570c54c
MJC <mjc@mjczone.com>
2020-01-25 17:44:08 -0600

    ServiceStack joins examples #97

---
+++ ORM Cookbook/Recipes.ServiceStack/Joins/JoinsTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Recipes.Joins;
+using Recipes.ServiceStack.Entities;
+
+namespace Recipes.ServiceStack.Joins
+{
+    [TestClass]
+    public class JoinsTests : JoinsTests<EmployeeDetail, Employee>
+    {
+        protected override IJoinsScenario<EmployeeDetail, Employee> GetScenario()
+        {
+            return new JoinsScenario(Setup.DbConnectionFactory);
+        }
+    }
+}
