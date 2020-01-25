2db465ed82232ba2fc224991755b0584ada90e52
MJC <mjc@mjczone.com>
2020-01-25 17:19:52 -0600

    ServiceStack stored proc examples #123

---
+++ ORM Cookbook/Recipes.ServiceStack/BasicStoredProc/BasicStoredProcTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Recipes.BasicStoredProc;
+using Recipes.ServiceStack.Entities;
+
+namespace Recipes.ServiceStack.BasicStoredProc
+{
+    [TestClass]
+    public class BasicStoredProcTests : BasicStoredProcTests<EmployeeClassification, EmployeeClassificationWithCount>
+    {
+        protected override IBasicStoredProcScenario<EmployeeClassification, EmployeeClassificationWithCount> GetScenario()
+        {
+            return new BasicStoredProcScenario(Setup.DbConnectionFactory);
+        }
+    }
+
+    [TestClass]
+    public class BasicStoredProcTests2 : BasicStoredProcTests<EmployeeClassification, EmployeeClassificationWithCount>
+    {
+        protected override IBasicStoredProcScenario<EmployeeClassification, EmployeeClassificationWithCount> GetScenario()
+        {
+            return new BasicStoredProcScenario2(Setup.DbConnectionFactory);
+        }
+    }
+}
