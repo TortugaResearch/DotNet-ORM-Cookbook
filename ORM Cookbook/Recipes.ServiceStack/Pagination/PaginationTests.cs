8b668fc6af45c22362ed6c95cfae32b8c26408a3
MJC <mjc@mjczone.com>
2020-01-25 19:42:35 -0600

    ServiceStack pagination examples #139

---
+++ ORM Cookbook/Recipes.ServiceStack/Pagination/PaginationTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Recipes.Pagination;
+using Recipes.ServiceStack.Entities;
+
+namespace Recipes.ServiceStack.Pagination
+{
+    [TestClass]
+    public class PaginationTests : PaginationTests<Employee>
+    {
+        protected override IPaginationScenario<Employee> GetScenario()
+        {
+            return new PaginationScenario(Setup.DbConnectionFactory);
+        }
+    }
+}
