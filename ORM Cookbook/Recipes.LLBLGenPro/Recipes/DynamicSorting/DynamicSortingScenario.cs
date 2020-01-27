79ab98325ba5311ede9d5a3fe46d2acd1c0a5df3
Frans Bouma <frans@sd.nl>
2020-01-27 16:00:26 +0100

    Fix for tab<->spaces

--- ORM Cookbook/Recipes.LLBLGenPro/Recipes/DynamicSorting/DynamicSortingScenario.cs
+++ ORM Cookbook/Recipes.LLBLGenPro/Recipes/DynamicSorting/DynamicSortingScenario.cs
@@ -9,43 +9,45 @@ using System.Linq;
 
 namespace Recipes.LLBLGenPro.DynamicSorting
 {
-    public class DynamicSortingScenario : IDynamicSortingScenario<EmployeeEntity>
-    {
-        public void InsertBatch(IList<EmployeeEntity> employees)
-        {
-            if (employees == null || employees.Count == 0)
-                throw new ArgumentException($"{nameof(employees)} is null or empty.", nameof(employees));
+	public class DynamicSortingScenario : IDynamicSortingScenario<EmployeeEntity>
+	{
+		public void InsertBatch(IList<EmployeeEntity> employees)
+		{
+			if(employees == null || employees.Count == 0)
+				throw new ArgumentException($"{nameof(employees)} is null or empty.", nameof(employees));
 
-            using (var toInsert = new EntityCollection<EmployeeEntity>(employees))
-            {
-                using (var adapter = new DataAccessAdapter())
-                {
-                    adapter.BatchSize = 100;
-                    adapter.SaveEntityCollection(toInsert);
-                }
-            }
-        }
+			using(var toInsert = new EntityCollection<EmployeeEntity>(employees))
+			{
+				using(var adapter = new DataAccessAdapter())
+				{
+					adapter.BatchSize = 100;
+					adapter.SaveEntityCollection(toInsert);
+				}
+			}
+		}
 
-        public IList<EmployeeEntity> SortBy(string lastName, string sortByColumn, bool isDescending)
-        {
-            using (var adapter = new DataAccessAdapter())
+
+		public IList<EmployeeEntity> SortBy(string lastName, string sortByColumn, bool isDescending)
+		{
+			using(var adapter = new DataAccessAdapter())
 			{
 				return new LinqMetaData(adapter).Employee.Where(x => x.LastName == lastName)
 												.OrderBy(sortByColumn, isDescending)
 												.ToList();
 			}
-        }
+		}
 
-        public IList<EmployeeEntity> SortBy(string lastName, string sortByColumnA, bool isDescendingA,
-            string sortByColumnB, bool isDescendingB)
-        {
-            using (var adapter = new DataAccessAdapter())
+
+		public IList<EmployeeEntity> SortBy(string lastName, string sortByColumnA, bool isDescendingA,
+											string sortByColumnB, bool isDescendingB)
+		{
+			using(var adapter = new DataAccessAdapter())
 			{
 				return new LinqMetaData(adapter).Employee.Where(x => x.LastName == lastName)
 												.OrderBy(sortByColumnA, isDescendingA)
 												.ThenBy(sortByColumnB, isDescendingB)
 												.ToList();
 			}
-        }
-    }
+		}
+	}
 }
--- ORM Cookbook/Recipes.LLBLGenPro/Recipes/Sorting/SortingScenario.cs
+++ ORM Cookbook/Recipes.LLBLGenPro/Recipes/Sorting/SortingScenario.cs
@@ -9,54 +9,57 @@ using System.Linq;
 
 namespace Recipes.LLBLGenPro.Sorting
 {
-    public class SortingScenario : ISortingScenario<EmployeeEntity>
-    {
-        public void InsertBatch(IList<EmployeeEntity> employees)
-        {
-            if (employees == null || employees.Count == 0)
-                throw new ArgumentException($"{nameof(employees)} is null or empty.", nameof(employees));
+	public class SortingScenario : ISortingScenario<EmployeeEntity>
+	{
+		public void InsertBatch(IList<EmployeeEntity> employees)
+		{
+			if(employees == null || employees.Count == 0)
+				throw new ArgumentException($"{nameof(employees)} is null or empty.", nameof(employees));
 
-            using (var toInsert = new EntityCollection<EmployeeEntity>(employees))
-            {
-                using (var adapter = new DataAccessAdapter())
-                {
-                    adapter.BatchSize = 100;
-                    adapter.SaveEntityCollection(toInsert);
-                }
-            }
-        }
+			using(var toInsert = new EntityCollection<EmployeeEntity>(employees))
+			{
+				using(var adapter = new DataAccessAdapter())
+				{
+					adapter.BatchSize = 100;
+					adapter.SaveEntityCollection(toInsert);
+				}
+			}
+		}
 
-        public IList<EmployeeEntity> SortByFirstName(string lastName)
-        {
-            using (var adapter = new DataAccessAdapter())
+
+		public IList<EmployeeEntity> SortByFirstName(string lastName)
+		{
+			using(var adapter = new DataAccessAdapter())
 			{
 				return new LinqMetaData(adapter).Employee.Where(x => x.LastName == lastName)
 												.OrderBy(x => x.FirstName)
 												.ToList();
 			}
-        }
+		}
 
-        public IList<EmployeeEntity> SortByMiddleNameDescFirstName(string lastName)
 
-        {
-            using (var adapter = new DataAccessAdapter())
+		public IList<EmployeeEntity> SortByMiddleNameDescFirstName(string lastName)
+
+		{
+			using(var adapter = new DataAccessAdapter())
 			{
 				return new LinqMetaData(adapter).Employee.Where(x => x.LastName == lastName)
 												.OrderByDescending(x => x.MiddleName)
 												.ThenBy(x => x.FirstName)
 												.ToList();
 			}
-        }
+		}
 
-        public IList<EmployeeEntity> SortByMiddleNameFirstName(string lastName)
-        {
-            using (var adapter = new DataAccessAdapter())
+
+		public IList<EmployeeEntity> SortByMiddleNameFirstName(string lastName)
+		{
+			using(var adapter = new DataAccessAdapter())
 			{
 				return new LinqMetaData(adapter).Employee
 												.Where(x => x.LastName == lastName)
 												.OrderBy(x => x.MiddleName).ThenBy(x => x.FirstName)
 												.ToList();
 			}
-        }
-    }
+		}
+	}
 }
