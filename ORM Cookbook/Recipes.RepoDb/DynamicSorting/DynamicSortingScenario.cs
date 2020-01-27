2101129d4e008810d31d3638f9f4900b07dc13e6
Jonathan Allen <grauenwolf@gmail.com>
2020-01-26 22:02:10 -0800

    Implement Dynamic Sorting for RepoDb #198

---
+++ ORM Cookbook/Recipes.RepoDb/DynamicSorting/DynamicSortingScenario.cs
@@ -0,0 +1,42 @@
+﻿using Recipes.DynamicSorting;
+using Recipes.RepoDb.Models;
+using RepoDb;
+using RepoDb.Enumerations;
+using RepoDb.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Recipes.RepoDb.DynamicSorting
+{
+    public class DynamicSortingScenario : BaseRepository<EmployeeSimple, SqlConnection>,
+        IDynamicSortingScenario<EmployeeSimple>
+    {
+        public DynamicSortingScenario(string connectionString)
+            : base(connectionString, ConnectionPersistency.Instance)
+        { }
+
+        public void InsertBatch(IList<EmployeeSimple> employees)
+        {
+            if (employees == null || employees.Count == 0)
+                throw new ArgumentException($"{nameof(employees)} is null or empty.", nameof(employees));
+
+            InsertAll(employees);
+        }
+
+        public IList<EmployeeSimple> SortBy(string lastName, string sortByColumn, bool isDescending)
+        {
+            var sort = new[] { new OrderField(sortByColumn, isDescending ? Order.Descending : Order.Ascending) };
+            return Query(x => x.LastName == lastName, orderBy: sort).AsList();
+        }
+
+        public IList<EmployeeSimple> SortBy(string lastName, string sortByColumnA, bool isDescendingA, string sortByColumnB, bool isDescendingB)
+        {
+            var sort = new[] {
+                new OrderField(sortByColumnA, isDescendingA ? Order.Descending : Order.Ascending),
+                new OrderField(sortByColumnB, isDescendingB ? Order.Descending : Order.Ascending)
+            };
+            return Query(x => x.LastName == lastName, orderBy: sort).AsList();
+        }
+    }
+}
---
+++ ORM Cookbook/Recipes.RepoDb/DynamicSorting/DynamicSortingTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Recipes.RepoDb.Models;
+using Recipes.DynamicSorting;
+
+namespace Recipes.RepoDb.DynamicSorting
+{
+    [TestClass]
+    public class DynamicSortingTests : DynamicSortingTests<EmployeeSimple>
+    {
+        protected override IDynamicSortingScenario<EmployeeSimple> GetScenario()
+        {
+            return new DynamicSortingScenario(Setup.ConnectionString);
+        }
+    }
+}
