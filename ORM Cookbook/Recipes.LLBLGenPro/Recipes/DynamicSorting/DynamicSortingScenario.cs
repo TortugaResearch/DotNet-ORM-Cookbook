5112490e731d7c8ba9c49c5ce011c64d4b6ed351
Frans Bouma <frans@sd.nl>
2020-01-28 16:32:57 +0100

    Dynamic Sorting updated for LLBLGen Pro to illustrate it does support sorting on a string.

--- ORM Cookbook/Recipes.LLBLGenPro/Recipes/DynamicSorting/DynamicSortingScenario.cs
+++ ORM Cookbook/Recipes.LLBLGenPro/Recipes/DynamicSorting/DynamicSortingScenario.cs
@@ -6,6 +6,10 @@ using Recipes.DynamicSorting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using LLBLGenPro.OrmCookbook.FactoryClasses;
+using SD.LLBLGen.Pro.ORMSupportClasses;
+using SD.LLBLGen.Pro.QuerySpec;
+using SD.LLBLGen.Pro.QuerySpec.Adapter;
 
 namespace Recipes.LLBLGenPro.DynamicSorting
 {
@@ -31,9 +35,22 @@ namespace Recipes.LLBLGenPro.DynamicSorting
 		{
 			using(var adapter = new DataAccessAdapter())
 			{
-				return new LinqMetaData(adapter).Employee.Where(x => x.LastName == lastName)
-												.OrderBy(sortByColumn, isDescending)
-												.ToList();
+				// We use queryspec here which can use the low-level API elements we need to use to accomplish sorting
+				// on a string. The low-level API uses entity fields and we obtain the field we need from a dummy entity instance
+				// we create. 
+				var fieldToSort = new EmployeeEntity().Fields[sortByColumn];
+				// fieldToSort will be null if sortByColumn isn't found. The query will therefore end in an exception if that's
+				// the case. For this particular test, it's the desired outcome so no null check is performed.
+				var q = new QueryFactory().Employee.Where(EmployeeFields.LastName.Equal(lastName))
+										  .OrderBy(isDescending
+													   ? fieldToSort.Descending()
+													   : fieldToSort.Ascending());
+				return (IList<EmployeeEntity>)adapter.FetchQuery(q);
+
+				// Alternatively we could have used the DynamicSortingExtensions in a linq query:
+				// return new LinqMetaData(adapter).Employee.Where(x => x.LastName == lastName)
+				// 								.OrderBy(sortByColumn, isDescending)
+				// 								.ToList();
 			}
 		}
 
